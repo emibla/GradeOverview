3479d9a569fb987510dc65a0c6e3125b7188151b
Emily Blakseth <ekblakse@stud.ntnu.no>
2021-03-29 13:51:46 +0200

    update test_class

--- project/src/main/java/test_package/test_class.java
+++ project/src/main/java/test_package/test_class.java
@@ -1,7 +1,13 @@
  package test_package;
 
 public class test_class {
+	
+	public void runTest() {
+		System.out.println("Running test");
+	}
 	public static void main(String[] args) {
-	System.out.println("Testing repo");
+		test_class test1 = new test_class();
+		test1.runTest();
+} 
 }
-}
+ 
