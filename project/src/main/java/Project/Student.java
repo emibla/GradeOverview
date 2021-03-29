7d299562a65bf296797551c2fd93406fe3aca62c
Emily Blakseth <ekblakse@stud.ntnu.no>
2021-03-29 15:16:58 +0200

    Create a student

---
+++ project/src/main/java/Project/Student.java
@@ -0,0 +1,39 @@
+package Project;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private int studentID;
+	
+	
+	/**
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param studentID
+	 */
+	public Student(String firstName, String lastName, int studentID) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.studentID = studentID;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public int getStudentID() {
+		return studentID;
+	}
+	
+	@Override
+	public String toString() {
+		return firstName + " " + lastName + " " + studentID;
+	}
+	
+	
+}
