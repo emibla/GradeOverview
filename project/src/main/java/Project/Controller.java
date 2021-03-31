b7a80083f64e038f8a9d283ad19d6aaaffe6deac
Emily Blakseth <ekblakse@stud.ntnu.no>
2021-03-31 20:22:28 +0200

    Methods for buttons, addStudent, searchStudent, addCourse and to see if any fields is empty

--- project/src/main/java/Project/Controller.java
+++ project/src/main/java/Project/Controller.java
@@ -1,14 +1,19 @@
 package Project;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -19,7 +24,7 @@ public class Controller {
 
 	// Register new Student
 	@FXML
-	public TextField firstname;
+	public TextField firstName;
 	@FXML
 	public TextField lastName;
 	@FXML
@@ -54,6 +59,11 @@ public class Controller {
 	public TextField courseYear;
 	@FXML
 	public Button registerCourseButton;
+	@FXML 
+	public ToggleGroup semesterToggle;
+	@FXML
+	public ToggleGroup gradeToggle;
+
 
 	// ErrorLabels
 	@FXML
@@ -94,12 +104,15 @@ public class Controller {
 	testCourse.registerNewCourse("TDT4100", "Høst", "2019", "A");
 	testCourse.registerNewCourse("TMA4100", "Vår", "2020", "C");
 	testCourse.registerNewCourse("TDT4245", "Høst", "2020", "B");
+	
+	//Henter ut data fra course ArrayList
+	courseTable.getItems().addAll(testCourse.getCourses());
 	}
 	
 	@FXML
 	private void initialize() {
-
 		fillWithTestData();
+		
 		try {
 
 			// oppretter kolonner i tableView
@@ -122,56 +135,150 @@ public class Controller {
 			List<Student> students = testStudents.getStudents();
 			System.out.println(students);
 			
-			//Henter ut data fra course ArrayList
+			//Når vi fjerner denne, vil det ikke vises testdata lenger
 			// courseTable.getItems().add(new Course("TDT4246", "Høst", "2020", "B"));
-			courseTable.getItems().addAll(testCourse.getCourses());
+			
 
 		} catch (NullPointerException n) {
 			System.out.println(n.getMessage());
 		}
+		
+		//Test. Printer ut studentene i konsollen
 		System.out.println(testStudents.toString());
-		searchStudentButton.setOnAction(actionEvent -> {
-			changeLabel();
-			loggedInStudentName();
-
+			
+		//knapp for å registrere student
+		registerStudentButton.setOnAction(actionEvent -> {
+			addStudent();
 		});
-
+		
+		//knapp for å søke etter student og sette navn på studenten i label
+		searchStudentButton.setOnAction(actionEvent -> {
+			loggedInStudentName();
+			searchStudent();
+		});
+		
+		//knapp for å registrere emne
+		registerCourseButton.setOnAction(actionEvent -> {
+			addCourse();
+		});
 	}
 
+	//Tester om buttonclick endrer label og henter fra textfield
 	@FXML
 	public void changeLabel() {
 		errorSearchStudent.setText(searchStudentID.getText());
+	}
+	
+	public void addStudent() {
+		StudentRegister studReg;
+		//lager et nytt objekt av typen StudentRegister
+		studReg = new StudentRegister();
+		errorRegisterStudent.setText(""); //setText og ikke = fordi det er en label og ikke en String 
+		if(ifAnyFieldRegisterStudentEmpty()) {
+			errorRegisterStudent.setText("Fyll alle felter");
+			return;
+		}
+		//registrerer det nye courset og legger det i testList
+		studReg.registerNewStudent(firstName.getText(), lastName.getText(), studentID.getText());
+		//Oppdaterer tabellen og getter alle course som ligger i testList på nytt
+		System.out.println(studReg.getStudents());
+	}
+	
+	//Søker etter student i students- arraylist
+	public void searchStudent() {
+		if(ifSearchFieldEmpty()) {
+			errorSearchStudent.setText("Tast inn studentID");
+			return;
+		}
 	}
 	
 	// Printer navnet til studenten når den er logget inn
 	@FXML
 	public void loggedInStudentName() {
 		String ID = searchStudentID.getText();
+		ArrayList<Student> stud = testStudents.findStudentByID(ID);
 		try {
-		loggedInStudentName.setText(testStudents.findStudentByID(ID).toString());
+		loggedInStudentName.setText("Velkommen "+ stud.get(0).getFirstName()+ " " + stud.get(0).getLastName());
 		}catch(Exception e) {
 			loggedInStudentName.setText("Not found");
 		}
 	}
-
-	// SøkButtonOnAction
-	@FXML
-	public void searchStudent_button_on_action() {
-		try {
-			// Hente ut verdien tastet inn i textfield og finne studenten med samme
-			// studentID i arrayList Student
-			String studentID = searchStudentID.getText();
-			System.out.println(studentID);
-
-		} catch (Exception e) {
-			errorSearchStudent.setText("Finner ikke student, er du sikker på at du har tastet rett StudentID?");
+	
+	//Metode for å hente ut informasjon fra registrering av course
+	public void addCourse() {
+		StudentRegister testList;
+		// lager en ny liste av klassen StudentRegister
+		testList = new StudentRegister();
+		errorRegisterCourse.setText("");
+		//hvis ifAnyFieldEmpty returnerer True, kastes en feilmelding i label og returnerer
+		if(ifAnyFieldEmpty())
+		{
+			errorRegisterCourse.setText("Fyll alle felter");
+			return;
 		}
+		String sem = "";
+		String grad = "";
+		
+		Toggle semesterTogger = semesterToggle.getSelectedToggle();
+		if (semesterTogger == semesterAutumn) {
+			sem = "Høst";			
+		}
+		else if (semesterTogger == semesterSpring) {
+			sem = "Vår";
+		}
+		
+		Toggle gradeTogger = gradeToggle.getSelectedToggle();
+		if (gradeTogger == gradeA) {
+			grad = "A";			
+		}
+		else if (gradeTogger == gradeB) {
+			grad = "B";
+		}
+		else if (gradeTogger == gradeC) {
+			grad = "C";
+		}		
+		else if (gradeTogger == gradeD) {
+			grad = "D";
+		}		
+		else if (gradeTogger == gradeE) {
+			grad = "E";
+		}
+	
+		//registrerer det nye courset og legger det i testList
+		testList.registerNewCourse(registerCourseID.getText(), sem, courseYear.getText(), grad);
+		//Oppdaterer tabellen og getter alle course som ligger i testList på nytt
+		courseTable.getItems().addAll(testList.getCourses());
+		updateTables();
+	}
+	
 
+	//Update table method som kjøres hver gang noen legger til et nytt course
+	public void updateTables() {
+		//something
+	}
+	
+	//Returnerer True hvis firstName, lastName eller studentID ikke er fylt ut
+	private boolean ifAnyFieldRegisterStudentEmpty() {
+		return firstName.getText().isEmpty()  || lastName.getText().isEmpty() || studentID.getText().isEmpty();
+				
+	}
+	
+	//Returnerer True hvis searchStudentID textField er empty
+	private boolean ifSearchFieldEmpty() {
+		return searchStudentID.getText().isEmpty();
+	}
+	
+	//returnerer True hvis registerCourseID eller courseYear eller en av togglebuttons ikke er fylt inn/presset
+	private boolean ifAnyFieldEmpty() {
+		return registerCourseID.getText().isEmpty()  || courseYear.getText().isEmpty() || (!semesterAutumn.isSelected() && !semesterSpring.isSelected()) ||
+				(!gradeA.isSelected() && !gradeB.isSelected() && !gradeC.isSelected() && !gradeD.isSelected() &&  !gradeE.isSelected());
 	}
 
 }
 
 /*
+ ******************************************************************************************************************
+ 
  * Lager en ny liste med Student av klassetypen Student, students er det jeg kaller den nye lista, testStudents er der dataen ligger, getSTudents er navnet på getteren i StudentRegister
  			List<Student> students = testStudents.getStudents();
 			System.out.println(students);
@@ -187,4 +294,18 @@ public void changeLabel() {
  * Course element = courses.get(i); } Course.setItems(listItems);
  * 
  * }
- */
+ * 	
+	// SøkButtonOnAction --- brukes ikke
+	@FXML
+	public void searchStudent_button_on_action() {
+		try {
+			// Hente ut verdien tastet inn i textfield og finne studenten med samme
+			// studentID i arrayList Student
+			String studentID = searchStudentID.getText();
+			System.out.println(studentID);
+
+		} catch (Exception e) {
+			errorSearchStudent.setText("Finner ikke student, er du sikker på at du har tastet rett StudentID?");
+		}
+
+	}*/
