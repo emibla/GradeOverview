6ae54abdcf10c105892d01b9debed814f52a441b
Emily Blakseth <ekblakse@stud.ntnu.no>
2021-04-20 00:27:39 +0200

    Update FileInterface

--- project/src/main/java/Project/FileInterface.java
+++ project/src/main/java/Project/FileInterface.java
@@ -4,8 +4,8 @@ import java.io.FileNotFoundException;
 
 public interface FileInterface {
 	
-	public void writeStudentsInRegister(String studentRegister);
+	public void writeStudentRegister(StudentRegister studentRegister, String filename);
 	
-	public void getStudentsFromRegister(String studentRegister) throws FileNotFoundException;
+	public void readStudentsFromFile(StudentRegister studentregister, String filename) throws FileNotFoundException;
 	
 }
