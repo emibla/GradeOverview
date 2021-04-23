434939eae6e951850ee07a55f0fc3d5c586fb5b3
Emily Blakseth <ekblakse@stud.ntnu.no>
2021-04-23 16:53:29 +0200

    Clean up FileManager

--- project/src/main/java/Project/FileManager.java
+++ project/src/main/java/Project/FileManager.java
@@ -1,11 +1,8 @@
 package Project;
 
-import java.io.File;
+import java.io.File; 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 
 public class FileManager implements FileInterface{
@@ -14,36 +11,6 @@ public class FileManager implements FileInterface{
 	 * Reads from and writes to the school Register 
 	 * Courses and Students
 	 */
-
-//	private List<Student> allStudents = new ArrayList<Student>();
-//	//private List<Course> allCourses = new ArrayList<Course>();
-//	
-//	static StudentRegister testList = new StudentRegister();
-//	
-//	static StudentRegister afterList = new StudentRegister();
-//	
-//
-//	
-//	public void addStudent(Student student) {
-//		allStudents.add(student);
-//		}
-
-	
-	/*
-	public void writeStudentsInRegister(String filename) {
-		
-		try {
-			PrintWriter writer = new PrintWriter(filename+".txt");		
-			for (Student student: allStudents) {
-			writer.println(student.toFullString());
-		}
-			writer.flush();
-			writer.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("tjohei"+e.getMessage());
-		}
-	}
-*/
 		
 		public void readStudentsFromFile(StudentRegister studentregister, String filename) throws FileNotFoundException {
 			Scanner scanner = new Scanner(new File(filename));
@@ -51,39 +18,20 @@ public class FileManager implements FileInterface{
 				String line = scanner.nextLine();
 				String[] lineInfo = line.split(",");
 				String[] splitInfo = line.split(";");
-				//System.out.println(splitInfo[1]);
-				//splitter course på ,
-				//System.out.println(line);
-				//ArrayList courseItems = new ArrayList();
-				//courseItems.add(courseLineInfo);
 				String firstName = lineInfo[0];
 				String lastName = lineInfo[1];
 				String studentID = lineInfo[2];
-				//Student student = new Student(firstName, lastName, studentID);
-				//System.out.println("");
-				//afterList.registerNewStudent(firstName, lastName, studentID);   ***** DENNE
 				studentregister.registerNewStudent(firstName, lastName, studentID);
 				if(splitInfo.length > 1) {
 					String[] courseLineInfo = splitInfo[1].split(",");
 					for(int i =0; i< courseLineInfo.length;) {
-					//System.out.println(studentID);  ******* OG DEN ØANGE UNDER
-					//afterList.findStudentByID(studentID).addCourse(new Course(courseLineInfo[i], courseLineInfo[i+1],Integer.parseInt(courseLineInfo[i+2]) , courseLineInfo[i+3]));
-					//System.out.println(courseLineInfo[i]);
-						studentregister.findStudentByID(studentID).addCourse(new Course(courseLineInfo[i], courseLineInfo[i+1],Integer.parseInt(courseLineInfo[i+2]) , courseLineInfo[i+3]));
+						studentregister.findStudentByID(studentID).addCourse(courseLineInfo[i], courseLineInfo[i+1],Integer.parseInt(courseLineInfo[i+2]) , courseLineInfo[i+3]);
 						i +=4;
 			}}
-			
-			
-		//	for(int i = 0; i < afterList.students.size(); i++) {
-		//		System.out.println("heipådei"+afterList.getStudents().get(i).toFullString());
-				
-			//}
 			} scanner.close();
 		}
 		
-
-
-		public void writeStudentRegister(StudentRegister studentRegister, String filename) {
+		public void writeStudentRegister(StudentRegister studentRegister, String filename)  {
 			
 			try {
 				PrintWriter writer = new PrintWriter(filename);		
@@ -94,7 +42,6 @@ public class FileManager implements FileInterface{
 				writer.flush();
 				writer.close();
 			} catch (FileNotFoundException e) {
-				System.out.println(e.getMessage());
 			}
 		}
 
@@ -172,8 +119,57 @@ public class FileManager implements FileInterface{
 //}
 //System.out.println(student);
 //this.addStudent(student);
+
+
+
+
+
+
+
+
+//private List<Student> allStudents = new ArrayList<Student>();
+////private List<Course> allCourses = new ArrayList<Course>();
+//
+//static StudentRegister testList = new StudentRegister();
+//
+//static StudentRegister afterList = new StudentRegister();
+//
+//
+//
+//public void addStudent(Student student) {
+//	allStudents.add(student);
+//	}
+
+
+/*
+public void writeStudentsInRegister(String filename) {
+	
+	try {
+		PrintWriter writer = new PrintWriter(filename+".txt");		
+		for (Student student: allStudents) {
+		writer.println(student.toFullString());
+	}
+		writer.flush();
+		writer.close();
+	} catch (FileNotFoundException e) {
+		System.out.println("tjohei"+e.getMessage());
+	}
+}
+
+
+
+
+//	for(int i = 0; i < afterList.students.size(); i++) {
+		//		System.out.println("heipådei"+afterList.getStudents().get(i).toFullString());
+			//}
+
+
+
+*/
 	
 	
 	
-	
+//System.out.println(studentID);  ******* OG DEN ØANGE UNDER
+//afterList.findStudentByID(studentID).addCourse(new Course(courseLineInfo[i], courseLineInfo[i+1],Integer.parseInt(courseLineInfo[i+2]) , courseLineInfo[i+3]));
+//System.out.println(courseLineInfo[i]);
 
